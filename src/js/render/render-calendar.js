a209bbaeb6b99672be3d7112980b099d22b1536f
Yaroslav <yaroslav6066@gmail.com>
2021-01-29 16:06:36 +0200

    fix event

--- src/js/render/render-calendar.js
+++ src/js/render/render-calendar.js
@@ -1,4 +1,4 @@
-const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
 const table = {
   time: [
     '10:00',
@@ -9,37 +9,37 @@ const table = {
     '15:00',
     '16:00',
     '17:00',
-    '18:00',
+    '18:00'
   ],
-  monday: [{ time: '10:00', user: 'Maria', title: 'One to one' }],
+  monday: [{ time: '10:00', user: 'Maria', title: 'One to one' }, { time: '12:00', user: 'Maria', title: 'One to one' }],
   tuesday: [],
   wednesday: [],
   thursday: [],
-  friday: [],
-}
+  friday: []
+};
 
 function createTable() {
-  const tableHead = document.getElementById('tableHead')
-  const tableBody = document.getElementById('tableBody')
-
+  const tableHead = document.getElementById('tableHead');
+  const tableBody = document.getElementById('tableBody');
   table.time.map((time) => {
+    let row = document.createElement('tr');
+    tableBody.append(row);
     days.map((day) => {
-      table[day].filter((item) =>
-        item.time === time
-          ? (tableBody.innerHTML += `<tr><td/>${item.title}</td></tr>`)
-          : (tableBody.innerHTML += `<tr><td/></td></tr>`)
-      )
-    })
-  })
+      // eslint-disable-next-line no-return-assign
+      table[day].filter((item) => item.time === time
+        ? (row.innerHTML += `<td/>${item.title}</td>`)
+        // eslint-disable-next-line quotes
+        : (row.innerHTML += `<td/></td>`));
+    });
+  });
 
   table.time.map(time => {
-    tableBody.innerHTML += `<tr><td/>${time}</td></tr>`
-  })
+    tableBody.innerHTML += `<tr><td/>${time}</td></tr>`;
+  });
 
   days.map((day) => {
-    tableHead.innerHTML += `<th>${day}</th>`
-  })
-
+    tableHead.innerHTML += `<th>${day}</th>`;
+  });
 }
 
-export default createTable
+export default createTable;
