5e1305f7a0029f026c6644d4179277155d9974ec
Yaroslav <yaroslav6066@gmail.com>
2021-02-02 13:56:28 +0200

    add name event to delete popup

--- src/js/render/toogle.js
+++ src/js/render/toogle.js
@@ -1,5 +1,6 @@
 const table = document.getElementById('table');
 let el = document.querySelector('.popup');
+let popupTitle = document.querySelector('.popup__title');
 
 export function toogle() {
   el.style.display = (el.style.display === 'none') ? 'flex' : 'none';
@@ -8,9 +9,12 @@ export function toogle() {
 document.querySelector('.popup__btn--no').addEventListener('click', () => {
   toogle();
 });
+
 // eslint-disable-next-line func-names
 table.onclick = function (event) {
   let target = event.target;
+  popupTitle.innerHTML = `Are you sure you want delete ${event.target.textContent} ?`;
+  console.log(event.target.textContent);
   if (target.tagName !== 'TD' || target.innerHTML === '') return;
   toogle();
 };
